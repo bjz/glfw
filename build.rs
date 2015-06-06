4871b081da9a1621f0fddb0fcdf5a69c3d8a0394
ceram1 <ceram1000@gmail.com>
2015-06-06 12:00:36 +0900

    Modified build.rs to support windows, and moved ffi to this crate.
    Supports msvc build & mingw, and build 64bit lib if target is 64bit.

--- build.rs
+++ build.rs
@@ -1,26 +1,80 @@
-use std::process::Command;
 use std::path::Path;
-use std::fs;
+use std::env;
+
+extern crate bt;
+use bt::cmd;
+
 
 fn main() {
-    Command::new("cmake")
-            .arg("-DCMAKE_C_FLAGS=-fPIC")
-            .arg("-DGLFW_BUILD_EXAMPLES=OFF")
-            .arg("-DGLFW_BUILD_TESTS=OFF")
-            .arg("-DGLFW_BUILD_DOCS=OFF")
-            .arg(env!("CARGO_MANIFEST_DIR"))
-            .current_dir(&Path::new(env!("OUT_DIR")))
-            .status()
-            .ok().expect("Failed to build with cmake");
+	let mut cflags = env::var("CFLAGS").unwrap_or(String::new());
 
-    Command::new("make")
-            .current_dir(&Path::new(env!("OUT_DIR")))
-            .status()
-            .ok().expect("Failed to build with make");
+	let host = env::var("HOST").unwrap();
 
-    fs::copy(&Path::new(env!("OUT_DIR")).join("src").join("libglfw3.a"),
-             &Path::new(env!("OUT_DIR")).join("libglfw3.a"))
-             .ok().expect("Failed to move file");
+	let target = env::var("TARGET").unwrap();
+	
+	let msvc = host.contains("windows") && target.contains("windows");
+	let mingw = !msvc && target.contains("windows");
 
-    println!("cargo:rustc-flags=-L {} -l glfw3:static", env!("OUT_DIR"));
+
+
+
+	if target.contains("i686") {
+		cflags.push_str(" -m32");
+	} else if target.contains("x86_64") {
+		cflags.push_str(" -m64");
+	}
+	if !target.contains("i686") {
+		cflags.push_str(" -fPIC");
+	}
+
+
+	let dst = env::var("OUT_DIR").unwrap();
+	let src_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+
+	// Generate 
+	cmd("cmake", |ref mut cmd| {
+		let profile = match &env::var("PROFILE").unwrap()[..] {
+			"bench" | "release" => "Release",
+			_ => "Debug",
+		};
+
+		if msvc {
+			// It's nessasary for 64bit build.
+			// vs 12 2014 requires 'CMAKE_C_COMPILER'
+			if target.contains("x86_64") { 
+				cmd.arg("-G").arg("Visual Studio 14 2015 Win64");
+			} else {
+				cmd.arg("-G").arg("Visual Studio 14 2015");
+			}
+		} else if mingw {
+			cmd.arg("-G").arg("MinGW Makefiles");
+		}
+		
+
+		cmd
+			.arg(&format!("-DCMAKE_BUILD_TYPE={}", profile))
+			.arg(&format!("-DCMAKE_C_FLAGS={}", cflags))
+			.arg("-DGLFW_BUILD_EXAMPLES=OFF")
+			.arg("-DGLFW_BUILD_TESTS=OFF")
+			.arg("-DGLFW_BUILD_DOCS=OFF")
+			.arg(&format!("-DCMAKE_INSTALL_PREFIX={}", dst))
+			.arg(src_dir)
+			.current_dir(&Path::new(&dst));
+	});
+
+
+
+	// Build
+	cmd("cmake", |ref mut cmd| {
+		cmd
+			.arg("--build").arg(".")
+			.arg("--target").arg("install")
+			.current_dir(&Path::new(&dst));
+	});
+	println!("cargo:root={}", dst);
+
+
+	println!("cargo:rustc-link-search={}", &Path::new(&dst).join("lib").display());
+    println!("cargo:rustc-link-lib=static=glfw3");
 }
